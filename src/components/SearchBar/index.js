66bd356b142909410c391f67deb90dd75c6dd45f
BenjaminCoajou <coajou.benjamin@gmail.com>
2021-06-22 17:59:01 +0200

    display infos with css

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -6,15 +6,16 @@ const SearchBar = ({inputChange, formSubmit, error}) => {
     
     return (
     
-        <div className="row">
-            <div className="col-md-6  mt-5">
+        <div className="row mx-5">
+            <div className="col-xl-3 col-md-6 mt-5">
                 <form onSubmit={(evt)  => {evt.preventDefault();formSubmit() }}>
                     <label htmlFor="inputCity" className="form-label display-4">City</label>
                     <input type="text" id="inputCity" className="form-control" placeholder="Search a city" onChange={(evt) => {inputChange(evt.target.value)}}/>
                 </form>
                 {error ? 
-                <div className="alert alert-danger">
-                    Incorect City
+                <div className="mt-2 d-flex align-items-baseline incorrect">
+                    <i class="fas fa-exclamation-triangle mx-2"></i>
+                    <p>Incorrect City</p>
                 </div> : "" }
             </div>
             
